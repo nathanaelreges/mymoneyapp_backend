2cf89187301ed4e2356ff35eec698ecc971c0f93
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-02 19:12:37 -0300

    Getting the summary

--- src/api/billingCycles/service.js
+++ src/api/billingCycles/service.js
@@ -9,7 +9,18 @@ billingCycle.route('count', (req, res, next) => {
    })
 })
 
-
+billingCycle.route('summary', (req, res, next) => {
+   billingCycle.aggregate([{
+      $project: {credits: {$sum: '$credits.value'}, debits: {$sum: '$debits.value'}}
+   },{
+      $group: {_id: null, credits: {$sum: '$credits'}, debits: {$sum: '$debits'}}
+   },{
+      $project: {_id: 0, credits: 1, debits: 1}
+   }])
+      .then((result)=>{
+         res.json(result)
+      })
+})
 
 module.exports = billingCycle
 
