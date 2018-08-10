72d074021f71da08d13b125908677ea3c03c3e14
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-10 10:05:26 -0300

    Error messages fix

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ mongoose.connect('mongodb://localhost:27017/billingcycle', {useNewUrlParser: tru
 mongoose.Promise = global.Promise
 
 
-mongoose.Error.messages.general.required = "O '{PATH}' precisa ser preenchido."
-mongoose.Error.messages.Number.min = "O valor '{VALUE}' informado é menor que o limite mínimo de '{MIN}' para o campo '{PATH}'"
-mongoose.Error.messages.Number.max = "O valor '{VALUE}' informado é maior que o limite máximo de '{MIN}' para o campo '{PATH}'"
-mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'"
+mongoose.Error.messages.general.required = 'O campo "{PATH}" precisa ser preenchido.'
+mongoose.Error.messages.Number.min = 'O valor "{VALUE}" informado é menor que o limite mínimo de "{MIN}" para o campo "{PATH}"'
+mongoose.Error.messages.Number.max = 'O valor "{VALUE}" informado é maior que o limite máximo de "{MIN}" para o campo "{PATH}"'
+mongoose.Error.messages.String.enum = 'O valor "{VALUE}" não é válido para o atributo "{PATH}"'
