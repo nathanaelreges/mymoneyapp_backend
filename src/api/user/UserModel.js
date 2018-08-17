8123f2ee0f1f5612e7cd3d295d3097bfcb657f9a
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-17 16:53:53 -0300

    Fixed bugs

--- src/api/user/UserModel.js
+++ src/api/user/UserModel.js
@@ -4,10 +4,8 @@ const mongoose = restful.mongoose
 
 const UserSchema = new mongoose.Schema({
    name: {type: String, required: true},
-   password: {type: String, required: true, min: 6, max: 24},
+   password: {type: String, required: true},
    email: {type: String, required: true}
 }) 
-
-
 
 module.exports = restful.model('user', UserSchema)
--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('./userModel')
 const secret = require('../../.env').authSecret
+const _ = require('lodash')
 
 
 
@@ -9,8 +10,9 @@ const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
 
 const sendDBerrors = (res, dbErrors) => {
-   const errors = dbErrors.map(error => (error.message))
-   res.status(400).json({ errors })
+   const errors = []
+   _.forIn(dbErrors.errors, error => errors.push(error.message))
+   return res.status(400).send({ errors })
 }
 
 
@@ -20,24 +22,20 @@ const login = (req, res, next) => {
    const password = req.body.password || ''
 
    User.findOne({ email }, async function (err, user) {
-      if(err){
-         sendDBerrors(res, err)
-      }
-      else
-      if(user) {
-         const match = await bcrypt.compare(password, user.password)
-         if(match) {
-            const { name, email } = user
-            const token = jwt.sign({ name, email }, secret, {expiresIn: '3 days'})
-            res.status(200).json({ token, name, email })
-         }
-         else {
-            res.json({errors: ['Senha/Usuário invalidos.']})
-         }
-      }
-      else {
-         res.json({errors: ['Senha/Usuário invalidos']})
-      }
+      if(err){ return sendDBerrors(res, err) }
+      
+      if(!user) { return res.status(400).send({errors: ['Senha/Usuário invalidos.']}) }
+
+      const match = await bcrypt.compare(password, user.password)
+
+      if(!match) { return res.status(400).send({errors: ['Senha/Usuário invalidos.']}) }
+
+      const { name, email } = user
+      const token = jwt.sign({ name, email }, secret, {expiresIn: '3 days'})
+      
+      return res.status(200).json({ token, name, email })
+      
+      
    })
 }
 
@@ -46,7 +44,7 @@ const validateToken = (req, res, next) => {
    const token = req.body.token || ''
 
    jwt.verify(token, secret, (err, decoded)=>{
-      res.status(200).json({ valid: !err })
+      return res.status(200).json({ valid: !err })
    })
 }
 
@@ -58,7 +56,7 @@ const sign = async function (req, res, next) {
    const confirmPassword = req.body.confirmPassword || ''
 
    if(!email.match(emailRegex)){
-      res.status(400).send({errors: ['O e-mail informado está inválido.']})
+      return res.status(400).send({errors: ['O e-mail informado está inválido.']})
    }
 
    /*if(!password.match(passwordRegex)){
@@ -66,36 +64,39 @@ const sign = async function (req, res, next) {
       'um número, uma caractere especial(@#$ %) e tamanho entre 6-20.']})
    }*/
 
-   User.findOne({ email },  async function (err, user) {
+   if(password.length < 4){
+      res.status(400).send({errors: ['Senha precisar ter: uma letra maiúscula, uma letra minúscula, ' +
+      'um número, uma caractere especial(@#$ %) e tamanho entre 6-20.']})
+   }
+
+   User.findOne({ email }, async function (err, user) {
       if(err){
-         sendDBerrors(res, err)
+         return sendDBerrors(res, err)
       }
-      else
+      
       if(user) {
-         res.json({errors: ['Usuário já cadastrado.']})
+         return res.status(400).send({errors: ['Usuário já cadastrado.']})
       }
-      else {
-         const passwordHash = await bcrypt.hash(password, 3)
-         const match = await bcrypt.compare(confirmPassword, passwordHash)
+      
+      const passwordHash = await bcrypt.hash(password, 3)
+      const match = await bcrypt.compare(confirmPassword, passwordHash)
+      
+      if(!match) {
+         return res.status(400).send({errors: ['Confirmação de senha invalida.']})
          
-         if(!match) {
-            res.json({errors: ['Confirmação de senha invalida.']})
-            return
+      }
+      
+      const newUser = new User({name, email, password: passwordHash})
+      newUser.save(err => {
+         if(err) {
+            return sendDBerrors(res, err)
          }
-         
-         const newUser = new User({name, email, password: passwordHash})
-         newUser.save(err => {
-            if(err) {
-               sendDBerrors(res, err)
-            }
-            else{
-               login(req, res, next)
-            }
-         })
-         
-         
-         
-      }
+         else{
+            login(req, res, next)
+         }
+      })
+      
+
    })
 
 }
